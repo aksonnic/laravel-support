f47695130d2ca516bec5b3cca47790797971c8cc
Andre Sonnichsen <andre@silvertipsoftware.com>
2023-01-16 13:21:31 -0800

    improve FluentModel test for Laravel version differences

--- tests/Eloquent/FluentModelTest.php
+++ tests/Eloquent/FluentModelTest.php
@@ -14,6 +14,20 @@ class FluentModelTest extends TestCase {
         ]);
     }
 
+    public function testAccessingProperties() {
+        $this->assertTrue(isset($this->device['model']));
+        $this->assertEquals('iPhone', $this->device->model);
+        $this->assertEquals('iPhone', $this->device['model']);
+    }
+
+    public function testSettingProperties() {
+        $this->device['model'] = 'Galaxy';
+        $this->assertEquals('Galaxy', $this->device->model);
+
+        $this->device->model = 'Pixel';
+        $this->assertEquals('Pixel', $this->device->model);
+    }
+
     public function testFluentModelComputedAttributes() {
         $this->assertEquals('Apple', $this->device->vendor);
     }
@@ -21,6 +35,9 @@ class FluentModelTest extends TestCase {
     public function testFluentModelMutators() {
         $this->device->type = '12 Pro Max';
         $this->assertEquals('12 Pro Max', $this->device->model);
+
+        $this->device['type'] = '14 Pro';
+        $this->assertEquals('14 Pro', $this->device->model);
     }
 
 
