b07063f8f9f7e93b4cc0e4365706bb45d587ac26
Andre Sonnichsen <andre@silvertipsoftware.com>
2023-01-22 16:07:36 -0800

    add humanize helper

---
+++ src/Blade/ModelHelpers.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace SilvertipSoftware\LaravelSupport\Blade;
+
+class ModelHelpers {
+
+    public static function registerDirectives() {
+        Blade::directive('humanize', function ($expression) {
+            return "<?php echo SilvertipSoftware\LaravelSupport\Blade\ModelHelpers::humanize($expression); ?>";
+        });
+    }
+
+    public static function humanize($modelOrClass, $attr = null, $options = []) {
+        $clz = is_string($modelOrClass) ? $modelOrClass : get_class($modelOrClass);
+
+        if ($attr == null) {
+            return $clz::modelName()->human($options);
+        }
+
+        return $clz::humanAttributeName($attr, $options);
+    }
+}
