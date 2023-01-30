3140a9a5d7e6e355ec03958f1f935aa3849fb5a3
Andre Sonnichsen <andre@silvertipsoftware.com>
2023-01-30 14:48:27 -0800

    add configbound

---
+++ src/Eloquent/ConfigBound.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace SilvertipSoftware\LaravelSupport\Eloquent;
+
+use Illuminate\Support\Arr;
+
+trait ConfigBound {
+
+    protected static $cachedInstances = null;
+
+    public static function find($key) {
+        return Arr::get(static::allInstances(), $key);
+    }
+
+    public static function exists($key) {
+        return array_key_exists($key, static::allInstances());
+    }
+
+    public static function all() {
+        return collect(static::allInstances());
+    }
+
+    protected static function allInstances() {
+        if (static::$cachedInstances == null) {
+            static::bootIfNotBooted();
+        }
+
+        return static::$cachedInstances;
+    }
+
+    protected static function bootIfNotBooted() {
+        if (static::$cachedInstances == null) {
+            static::loadModels();
+        }
+    }
+
+    protected static function loadModels() {
+        foreach (config(static::$configKey) as $key => $attrs) {
+            static::$cachedInstances[$key] = new static(['id' => $key] + $attrs);
+        }
+    }
+}
